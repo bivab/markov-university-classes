0d1c8dd4072c857f53e52bd91825faab30a7b965
David Schneider <david.schneider@bivab.de>
2017-07-22 11:44:18 +0200

    Moved data to a per-dataset subfolder

--- gen.py
+++ gen.py
@@ -6,13 +6,16 @@ import markovify
 
 STATE_SIZE = 1
 BATCH_SIZE = 5
+DATASET = "phil"
 
 
 def main():
-    with codecs.open('data/units.txt', 'r', 'utf-8') as f:
+    units = 'data/{0}/units.txt'.format(DATASET)
+    abstract_units = 'data/{0}/abstract_units.txt'.format(DATASET)
+    with codecs.open(units, 'r', 'utf-8') as f:
         text = f.read()
     model1 = markovify.NewlineText(text, state_size=STATE_SIZE)
-    with codecs.open('data/abstract_units.txt', 'r', 'utf-8') as f:
+    with codecs.open(abstract_units, 'r', 'utf-8') as f:
         text =f.read()
     model2 = markovify.NewlineText(text, state_size=STATE_SIZE)
 
@@ -27,11 +30,16 @@ def main():
 
     print("\n----------------\n")
 
-    for i in range(BATCH_SIZE):
-        print(model.make_sentence_with_start("Die"))
+    try:
+        for i in range(BATCH_SIZE):
+            print(model.make_sentence_with_start("Die"))
+    except KeyError:
+        pass
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         STATE_SIZE = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        DATASET = sys.argv[2]
 
     main()
